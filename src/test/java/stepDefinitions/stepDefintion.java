da4f09473485273aadc87536ff0fac39a71fa6e6
rajesh9487@gmail.com <rajesh9487@gmail.com>
2023-12-21 20:56:35 +0530

    commit

---
+++ src/test/java/stepDefinitions/stepDefintion.java
@@ -0,0 +1,41 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class stepDefintion {
+	
+	@Given("Add place payload with {string} {string} {string}")
+	public void add_place_payload_with(String string, String string2, String string3) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("user calls {string} with {string} http request")
+	public void user_calls_with_http_request(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the API call got success with status code {int}")
+	public void the_api_call_got_success_with_status_code(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("{string} in response body is {string}")
+	public void in_response_body_is(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("verify place_Id created maps to {string} using {string}")
+	public void verify_place_id_created_maps_to_using(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+
+}
