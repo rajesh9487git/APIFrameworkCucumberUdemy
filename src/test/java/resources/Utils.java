e5f1f23e01eb48bbc73027a03c672cff478e8420
rajesh9487@gmail.com <rajesh9487@gmail.com>
2023-12-23 18:29:01 +0530

    updated

---
+++ src/test/java/resources/Utils.java
@@ -0,0 +1,18 @@
+package resources;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+
+public class Utils {
+	
+	public static RequestSpecification req;
+//	
+//	public void requestSpecification() {
+//		
+//		if(req==null) {
+//			
+//			req= new RequestSpecBuilder().setBaseUri(null)
+//		}
+//	}
+//
+}
