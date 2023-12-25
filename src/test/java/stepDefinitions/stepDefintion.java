90b95a33f49f8652d963be1124287d9ff5126ec9
rajesh9487@gmail.com <rajesh9487@gmail.com>
2023-12-25 18:09:41 +0530

    updating

--- src/test/java/stepDefinitions/stepDefintion.java
+++ src/test/java/stepDefinitions/stepDefintion.java
@@ -1,40 +1,77 @@
 package stepDefinitions;
+import static io.restassured.RestAssured.given;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.*;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-
-public class stepDefintion {
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import pojo.AddPlace;
+import pojo.Location;
+import resources.APIResources;
+import resources.TestDataBuild;
+import resources.Utils;
+public class stepDefintion extends Utils{
+	
+	RequestSpecification res;
+	ResponseSpecification resspec;
+	Response response;
+	TestDataBuild data = new TestDataBuild();
+	static String place_id;
 	
 	@Given("Add place payload with {string} {string} {string}")
-	public void add_place_payload_with(String string, String string2, String string3) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void add_place_payload_with(String name, String language, String address) throws IOException {
+	
+		 res=given().spec(requestSpecification())
+		.body(data.addPlacePayload(name,language,address));
+		
 	}
 
 	@When("user calls {string} with {string} http request")
-	public void user_calls_with_http_request(String string, String string2) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void user_calls_with_http_request(String resource, String method) {
+	    
+		
+		APIResources resourceAPI=  APIResources.valueOf(resource);
+		resspec= new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
+		
+		if(method.equalsIgnoreCase("POST"))
+			response=res.when().post(resourceAPI.getResource());
+		else if(method.equalsIgnoreCase("GET"))
+			response=res.when().get(resourceAPI.getResource());
+		
+		
 	}
 
 	@Then("the API call got success with status code {int}")
 	public void the_api_call_got_success_with_status_code(Integer int1) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    assertEquals(response.getStatusCode(),200);
 	}
 
 	@And("{string} in response body is {string}")
-	public void in_response_body_is(String string, String string2) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void in_response_body_is(String keyValue, String Expectedvalue) {
+	  
+		assertEquals(getJsonPath(response, keyValue),Expectedvalue );
 	}
 
 	@And("verify place_Id created maps to {string} using {string}")
 	public void verify_place_id_created_maps_to_using(String string, String string2) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	   
+		
+		place_id=getJsonPath(response,"place_id");
+//		res=given
 	}
 
 
